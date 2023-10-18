943055c7bd0ab187f5331e9b7537f606ff0e1d50
Torben <59419684+entorb@users.noreply.github.com>
2023-10-19 00:30:59 +0200

    Added DataTable and TooltipSpeed

---
+++ src/components/TooltipSpeed.vue
@@ -0,0 +1,22 @@
+<template>
+  {{ ips.toPrecision(3) }}
+  <v-tooltip activator="parent">
+    {{ ips.toPrecision(3) }}/sec<br />
+    {{ (ips * 60).toPrecision(3) }}/min<br />
+    {{ (ips * 3600).toPrecision(3) }}/hour<br />
+    {{ (ips * 3600 * 24).toPrecision(3) }}/day
+  </v-tooltip>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'TooltipSpeed',
+  props: {
+    ips: {
+      type: Number,
+      required: true,
+    },
+  },
+})
+</script>
