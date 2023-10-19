6ba9cd8e6329e3bf0af849213ebfb6784d3d45ab
Torben <59419684+entorb@users.noreply.github.com>
2023-10-19 04:29:12 +0200

    Stats calculated and refactored

--- src/components/helper.ts
+++ src/components/helper.ts
@@ -12,3 +12,23 @@ export const helperDateToString = (datetime: Date, showDays: boolean = false): s
   }
   return datetime.toLocaleString('de-DE', options)
 }
+
+export const helperSecondsToString = (totalSeconds: number): string => {
+  totalSeconds = Math.round(totalSeconds)
+  const days = Math.floor(totalSeconds / 86400)
+  const hours = Math.floor((totalSeconds % 86400) / 3600)
+  const minutes = Math.floor((totalSeconds % 3600) / 60)
+  const seconds = totalSeconds % 60
+
+  if (days > 0) {
+    return `${days}d ${hours}h ${minutes}m`
+  } else if (hours > 0) {
+    return `${hours}h ${minutes}m`
+  } else if (minutes >= 15) {
+    return `${minutes}m`
+  } else if (minutes > 0) {
+    return `${minutes}m ${seconds}s`
+  } else {
+    return `${seconds}s`
+  }
+}
