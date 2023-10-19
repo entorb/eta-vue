ff66bdd2bb87fef8682574ada4bb20671d40f906
Torben <59419684+entorb@users.noreply.github.com>
2023-10-19 18:30:38 +0200

    plus-1 button

---
+++ src/components/Actions.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-btn icon="mdi-numeric-positive-1" v-on:click="$emit('plus-1')"></v-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  emits: ['plus-1'],
+  props: {
+    data: { type: Array<{ date: Date; value: number }>, default: [] },
+  },
+})
+</script>
