83aad83c32d2fb8ef04ce3c193b524abb9b28c28
Torben <59419684+entorb@users.noreply.github.com>
2023-10-20 19:10:04 +0200

    build: add test prior to deployment

--- scripts/publish_to_web.sh
+++ scripts/publish_to_web.sh
@@ -3,4 +3,4 @@
 # ensure we are in the root dir
 script_dir=$(cd $(dirname $0) && pwd)
 cd $script_dir/..
-npm run format && npm run lint && npm run build && rsync -rvhu --delete --no-perms dist/* entorb@entorb.net:html/eta-vue/
+npm run format && npm run lint && npm run test-once && npm run build && rsync -rvhu --delete --no-perms dist/* entorb@entorb.net:html/eta-vue/
