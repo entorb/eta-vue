7cf70a681cd50cd39216265ead6779ff9644e93e
Torben <59419684+entorb@users.noreply.github.com>
2023-10-20 16:25:51 +0200

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+// idea
+
+// type DataRow = {
+//   date: Date
+//   value: Number
+// }
+
+// interface Props {
+//   data: { [date: string]: number }
+// }
+// props: { data: { type: Object, default: {} } as PropType<Props['data']>, }
