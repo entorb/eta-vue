a8d88eb52164917e8a3c9574d5b4fc4073cc4e67
Torben <59419684+entorb@users.noreply.github.com>
2023-10-20 02:07:44 +0200

    eslint missing package

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,13 @@ module.exports = {
     node: true,
   },
   extends: [
-    'google',
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
-    'prettier', // as last one
   ],
   rules: {
     'vue/multi-word-component-names': 'off',
-    quotes: ['warn', 'double'],
   },
   parserOptions: {
     ecmaVersion: 'latest',
