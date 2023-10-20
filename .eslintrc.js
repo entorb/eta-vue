c2b14c57929ce67688a547b829bf5523ded2bf42
Torben <59419684+entorb@users.noreply.github.com>
2023-10-20 04:15:59 +0200

    Add vitest for unit tests and adjusting eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,23 @@ module.exports = {
     node: true,
   },
   extends: [
+    'google',
     'plugin:vue/vue3-recommended',
-    'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
   ],
+  plugins: ['vitest'],
   rules: {
     'vue/multi-word-component-names': 'off',
+    'vue/valid-attribute-name': 'error',
   },
   parserOptions: {
     ecmaVersion: 'latest',
   },
+  overrides: [
+    {
+      files: ['**/*.spec.js'],
+      extends: ['plugin:vitest/recommended'],
+    },
+  ],
 }
