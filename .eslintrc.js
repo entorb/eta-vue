ddeb230c3eb53592b4efdb70bce92e80729b1fd7
Torben <59419684+entorb@users.noreply.github.com>
2023-10-20 17:42:24 +0200

    sync project setup from testing-tutorial via npm init vue@latest
    https://codingpr.com/test-your-vue-3-app-with-vitest-and-vue-test-utils/

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,15 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
   extends: [
-    'google',
-    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
   ],
-  plugins: ['vitest'],
-  rules: {
-    'vue/multi-word-component-names': 'off',
-    'vue/valid-attribute-name': 'error',
-  },
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  overrides: [
-    {
-      files: ['**/*.spec.js'],
-      extends: ['plugin:vitest/recommended'],
-    },
-  ],
 }
---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
+
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      server: {
+        deps: {
+          inline: ["vuetify"],
+        }
+      },
+      exclude: [...configDefaults.exclude, 'vorlagen/**'],
+      root: fileURLToPath(new URL('./', import.meta.url))
+    }
+  })
+)
