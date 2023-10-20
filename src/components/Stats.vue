2a0ed1509d3ec5231fac26d4d53e904135c91711
Torben <59419684+entorb@users.noreply.github.com>
2023-10-20 02:08:00 +0200

    eslint + prettier findings

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -3,34 +3,41 @@
     <tbody>
       <tr>
         <th>Total speed</th>
-        <td v-if="slope !== 0.0"><TooltipSpeed :ips="slope"></TooltipSpeed></td>
-      </tr>
-      <tr v-if="target != undefined">
-        <th>ETA</th>
-        <td>{{ dateToString(eta) }}</td>
+        <td v-if="itemsPerSecSlope !== 0.0">
+          <TooltipSpeed :ips="itemsPerSecSlope"></TooltipSpeed>
+        </td>
       </tr>
       <tr>
         <th>Start</th>
         <td>{{ dateToString(firstDate) }}</td>
       </tr>
-      <tr v-if="target != undefined">
-        <th>Time to go</th>
-        <td>{{ secToString(secToGo) }}</td>
-      </tr>
       <tr>
         <th>Runtime</th>
-        <td>{{ secToString(runtime) }}</td>
+        <td>{{ secToString(timeSinceFirstValue) }}</td>
+      </tr>
+      <tr v-if="target != undefined">
+        <th>ETA</th>
+        <td>{{ dateToString(eta) }}</td>
+      </tr>
+      <tr v-if="target != undefined">
+        <th>Time to go</th>
+        <td>{{ secToString(timeToETA) }}</td>
       </tr>
       <tr v-if="target != undefined">
         <th>Percent</th>
-        <td>{{ percent.toFixed(0) }}%</td>
+        <td>{{ (100 * percentOfTarget).toFixed(1) }}%</td>
+      </tr>
+      <tr v-if="target != undefined">
+        <th>Percent (est)</th>
+        <td>{{ (100 * percentOfTargetEstimated).toFixed(1) }}%</td>
       </tr>
     </tbody>
   </v-table>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent } from 'vue'
+// onMounted, onBeforeUnmount
 import { helperDateToString, helperSecondsToString } from './helper'
 import { helper_lin_reg } from './helper_lin_reg'
 import TooltipSpeed from './TooltipSpeed.vue'
@@ -45,19 +52,19 @@ export default defineComponent({
   data() {
     return {
       firstDate: new Date(0),
-      firstValue: 0.0,
       lastDate: new Date(0),
+      firstValue: 0.0,
       lastValue: 0.0,
-      percent: 0.0,
-      percent_since_first: 0.0,
-      runtime: 0.0,
-      secToGo: 0,
-      slope: 0.0,
+      percentOfTarget: 0.0,
+      percentOfTargetEstimated: 0.0,
+      timeSinceFirstValue: 0.0,
+      timeToETA: 0,
+      itemsPerSecSlope: 0.0,
       eta: new Date(0),
+      timerInterval: null as NodeJS.Timeout | null,
+      itemsDone: 0,
+      itemsTotal: 0,
     }
-  },
-  created() {
-    this.updateStats()
   },
   watch: {
     data: {
@@ -66,11 +73,18 @@ export default defineComponent({
     },
     target: { handler: 'updateStats' },
   },
+  created() {
+    this.updateStats()
+    // also starts timer
+  },
+  beforeUnmount() {
+    this.stopTimer()
+  },
   methods: {
     dateToString(datetime: Date): string {
-      const firstDate = this.data[0].date
-      const lastDate = this.data[this.data.length - 1].date
-      const showDays = Number(firstDate) - Number(lastDate) > 1000 * 60 * 60 * 24
+      // decide if we need to show days
+      // TODO: move to App settings
+      const showDays = this.firstDate.getTime() - this.lastDate.getTime() > 1000 * 86400 // 60 * 60 * 24
       return helperDateToString(datetime, showDays)
     },
     secToString(sec: number): string {
@@ -85,29 +99,73 @@ export default defineComponent({
       this.firstValue = this.data[0].value
       this.lastDate = this.data[this.data.length - 1].date
       this.lastValue = this.data[this.data.length - 1].value
-      const { slope, intercept } = helper_lin_reg(this.data, true)
-      this.slope = slope
-      let seconds_till_target = 0
-      if (this.target === undefined) {
-        return
+
+      const { slope } = helper_lin_reg(this.data, true)
+      this.itemsPerSecSlope = slope
+      if (this.target !== undefined) {
+        this.itemsDone = this.target != 0 ? this.lastValue : this.firstValue - this.lastValue
+        this.itemsTotal = this.target != 0 ? this.target : this.firstValue
+        this.percentOfTarget = this.itemsDone / this.itemsTotal
+
+        if (this.itemsPerSecSlope != 0) {
+          if (this.itemsTotal > this.itemsDone) {
+            if (this.target != 0) {
+              this.timeToETA = (this.itemsTotal - this.itemsDone) / this.itemsPerSecSlope
+            } else {
+              this.timeToETA = (this.itemsTotal - this.itemsDone) / -this.itemsPerSecSlope
+            }
+          }
+        }
+        this.eta = new Date(this.lastDate.getTime() + this.timeToETA * 1000)
       }
-      if (this.target > this.lastValue && this.slope != 0) {
-        seconds_till_target = (this.target - this.lastValue) / this.slope
+
+      this.updateTimes()
+      if (new Date().getTime() < this.eta.getTime()) {
+        this.startTimer()
       }
-      this.eta = new Date(this.lastDate.getTime() + seconds_till_target * 1000)
-      if (this.target != 0) {
-        this.percent = (100 * this.lastValue) / this.target
-        this.percent_since_first =
-          (100 * (this.lastValue - this.firstValue)) / (this.target - this.firstValue)
+    },
+    updateTimes() {
+      // executed by interval timer
+      const now = new Date().getTime()
+      this.timeSinceFirstValue = Math.round((now - this.firstDate.getTime()) / 1000)
+
+      // time < eta
+      if (new Date().getTime() < this.eta.getTime()) {
+        this.timeToETA = Math.round((this.eta.getTime() - now) / 1000)
+        if (this.target !== undefined) {
+          const timeSinceLastValue = (now - this.lastDate.getTime()) / 1000
+          if (this.target != 0) {
+            this.percentOfTargetEstimated =
+              (this.itemsDone + this.itemsPerSecSlope * timeSinceLastValue) / this.itemsTotal
+          } else {
+            this.percentOfTargetEstimated =
+              (this.itemsDone - this.itemsPerSecSlope * timeSinceLastValue) / this.itemsTotal
+          }
+        }
       } else {
-        this.percent = (100 * (this.firstValue - this.lastValue)) / this.firstValue
+        // if time > eta we stop the timer
+        this.stopTimer()
+        this.timeToETA = 0
+        this.percentOfTargetEstimated = 1 // 100%
       }
-      // TODO: these should be updated by setInterval
-      this.runtime = Math.round((new Date().getTime() - this.firstDate.getTime()) / 1000)
-      if (this.eta.getTime() > new Date().getTime()) {
-        this.secToGo = Math.round((this.eta.getTime() - new Date().getTime()) / 1000)
-      } else {
-        this.secToGo = 0
+    },
+    // create a timer, that updates the elapsed and remaining time periodically
+    startTimer() {
+      this.stopTimer()
+      let sleep: number = 1
+      if ((this.timeToETA == 0 || this.timeToETA > 15 * 60) && this.timeSinceFirstValue > 15 * 60) {
+        sleep = 30
+      }
+
+      this.timerInterval = setInterval(() => {
+        this.updateTimes()
+      }, sleep * 1000) // sleep time in ms
+    },
+    stopTimer() {
+      // Stop the timer
+      if (this.timerInterval) {
+        clearInterval(this.timerInterval)
+        this.timerInterval = null
       }
     },
   },
