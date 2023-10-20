0d79945d86e7d944cd0051347c68c53ff701085a
Torben <59419684+entorb@users.noreply.github.com>
2023-10-20 04:43:55 +0200

    UnitTests for math and some minor fixes

---
+++ src/components/DataTable.spec.js
@@ -0,0 +1,32 @@
+import { shallowMount } from '@vue/test-utils'
+import DataTable from '../components/DataTable.vue'
+
+describe('testing DataTable', () => {
+  it('calculates items per second when index is positive', () => {
+    const data = [
+      { date: new Date('2023-10-17T12:00:00'), value: 100 },
+      { date: new Date('2023-10-17T12:01:00'), value: 130 },
+      { date: new Date('2023-10-17T12:02:00'), value: 200 },
+    ]
+
+    const wrapper = shallowMount(DataTable, {
+      props: {
+        data,
+      },
+    })
+    const itemsPerSec = wrapper.vm.calculateItemsPerSecFromPreviousLine(1)
+    expect(itemsPerSec).toBe(0.5)
+  })
+
+  it('empty data', () => {
+    const data = [{ date: new Date('2023-10-17T12:00:00'), value: 100 }]
+
+    const wrapper = shallowMount(DataTable, {
+      props: {
+        data,
+      },
+    })
+    const itemsPerSec = wrapper.vm.calculateItemsPerSecFromPreviousLine(0)
+    expect(itemsPerSec).toBe(0)
+  })
+})
