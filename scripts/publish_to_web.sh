afbe6fc5724ef33c26cc692b7f0594572b21ceac
Torben <59419684+entorb@users.noreply.github.com>
2023-10-21 20:50:41 +0200

    Update publish_to_web.sh: remove format, lint, spell for now

--- scripts/publish_to_web.sh
+++ scripts/publish_to_web.sh
@@ -4,6 +4,7 @@
 script_dir=$(cd $(dirname $0) && pwd)
 cd $script_dir/..
 
-npm run format && npm run lint && npm run test-once && npm run build && rsync -rvhu --delete --no-perms dist/* entorb@entorb.net:html/eta-vue/
+# npm run format && npm run lint && npm run test-once &&
+#     npm run build && rsync -rvhu --delete --no-perms dist/* entorb@entorb.net:html/eta-vue/
 
-# npm run build && rsync -rvhu --delete --no-perms dist/* entorb@entorb.net:html/eta-vue/
+npm run build && rsync -rvhu --delete --no-perms dist/* entorb@entorb.net:html/eta-vue/
