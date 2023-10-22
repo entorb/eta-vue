2e14da658a062157cbcdff86fab9ba603bbec351
Torben <59419684+entorb@users.noreply.github.com>
2023-10-22 09:19:12 +0200

    Update TooltipSpeed.vue to <script setup

--- src/components/TooltipSpeed.vue
+++ src/components/TooltipSpeed.vue
@@ -1,55 +1,47 @@
 <template>
-  {{ speedInUnit(ips) }}
+  {{ speedInUnit }}
   <v-tooltip activator="parent">
-    {{ ipsInSec(ips) }}<br />
-    {{ ipsInMin(ips) }}<br />
-    {{ ipsInHour(ips) }}<br />
-    {{ ipsInDay(ips) }}
+    {{ ipsInUnit('sec') }}<br />
+    {{ ipsInUnit('min') }}<br />
+    {{ ipsInUnit('hour') }}<br />
+    {{ ipsInUnit('day') }}
   </v-tooltip>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { ref, watch } from 'vue'
 import { helperValueToString } from './helper'
 
-export default defineComponent({
-  name: 'TooltipSpeed',
-  props: {
-    ips: {
-      // items per second
-      type: Number,
-      required: true
-    },
-    unit: {
-      //sec, min, hour, day
-      type: String,
-      required: true
-    }
+const props = defineProps({
+  ips: {
+    // items per second
+    type: Number,
+    required: true
   },
-  methods: {
-    speedInUnit(ips: number): String {
-      if (this.unit == 'sec') {
-        return this.ipsInSec(ips)
-      } else if (this.unit == 'min') {
-        return this.ipsInMin(ips)
-      } else if (this.unit == 'hour') {
-        return this.ipsInHour(ips)
-      } else {
-        return this.ipsInDay(ips)
-      }
-    },
-    ipsInSec(ips: number): String {
-      return helperValueToString(ips) + '/sec'
-    },
-    ipsInMin(ips: number): String {
-      return helperValueToString(ips * 60) + '/min'
-    },
-    ipsInHour(ips: number): String {
-      return helperValueToString(ips * 3600) + '/h'
-    },
-    ipsInDay(ips: number): String {
-      return helperValueToString(ips * 86400) + '/day'
-    }
+  unit: {
+    // sec, min, hour, day
+    type: String,
+    required: true
   }
 })
+
+const speedInUnit = ref('')
+
+const ipsInUnit = (unit: string): string => {
+  const factor = {
+    sec: 1,
+    min: 60,
+    hour: 3600,
+    day: 86400
+  }[unit] as Record<string, number>
+  return helperValueToString(props.ips * factor) + '/' + unit.charAt(0)
+}
+
+watch(
+  () => props.unit,
+  () => {
+    speedInUnit.value = ipsInUnit(props.unit)
+  },
+  { immediate: true }
+)
 </script>
