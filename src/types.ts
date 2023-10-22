01a166fe50403d6f59a01aca284a4e6133704b99
Torben <59419684+entorb@users.noreply.github.com>
2023-10-22 13:15:54 +0200

    def central UnitType

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,1 @@
-// idea
-
-// type DataRow = {
-//   date: Date
-//   value: Number
-// }
-
-// interface Props {
-//   data: { [date: string]: number }
-// }
-// props: { data: { type: Object, default: {} } as PropType<Props['data']>, }
+export type UnitType = 'sec' | 'min' | 'hour' | 'day'
