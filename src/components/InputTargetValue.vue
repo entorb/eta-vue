9232ac98070eae3d87d5db9411ea3dac7a877989
Torben <59419684+entorb@users.noreply.github.com>
2023-10-22 11:59:00 +0200

    Update InputTargetValue.vue  to <script setup

--- src/components/InputTargetValue.vue
+++ src/components/InputTargetValue.vue
@@ -32,56 +32,54 @@
   </v-container>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+const props = defineProps({
+  target: { type: Number || undefined, default: undefined }
+})
 
-export default defineComponent({
-  name: 'InputTargetValue',
-  props: { target: { type: Number || undefined, default: undefined } },
-  emits: ['set-target', 'add-row'],
-  data() {
-    return {
-      inputTarget: '',
-      inputValue: ''
-    }
-  },
-  watch: {
-    target: { handler: 'updateInputFieldFromTarget' }
-  },
-  methods: {
-    enterTarget() {
-      let target = undefined
-      if (this.inputTarget == '') {
-        target = undefined
-      } else {
-        target = parseFloat(this.inputTarget.replace(',', '.'))
-        if (isNaN(target)) {
-          this.inputTarget = ''
-          return
-        }
-      }
-      if (target != this.target) {
-        this.$emit('set-target', target)
-      }
-    },
-    enterValue() {
-      const value = parseFloat(this.inputValue.replace(',', '.'))
-      if (isNaN(value)) {
-        return
-      }
-      const date = new Date()
-      const newRow = { date: date, value: value }
-      // Emit an event to add the row to the data
-      this.$emit('add-row', newRow)
-      this.inputValue = ''
-    },
-    updateInputFieldFromTarget() {
-      if (this.target !== undefined) {
-        this.inputTarget = this.target.toString()
-      } else {
-        this.inputTarget = ''
-      }
+const emits = defineEmits(['set-target', 'add-row'])
+
+const inputTarget = ref('')
+const inputValue = ref('')
+
+const targetRef = ref(props.target)
+
+watch(targetRef, () => {
+  updateInputFieldFromTarget()
+})
+
+function enterTarget() {
+  let target = undefined
+  if (inputTarget.value == '') {
+    target = undefined
+  } else {
+    target = parseFloat(inputTarget.value.replace(',', '.'))
+    if (isNaN(target)) {
+      inputTarget.value = ''
+      return
     }
   }
-})
+  if (target != props.target) {
+    emits('set-target', target)
+  }
+}
+function enterValue() {
+  const value = parseFloat(inputValue.value.replace(',', '.'))
+  if (isNaN(value)) {
+    return
+  }
+  const date = new Date()
+  const newRow = { date: date, value: value }
+  // Emit an event to add the row to the data
+  emits('add-row', newRow)
+  inputValue.value = ''
+}
+function updateInputFieldFromTarget() {
+  if (props.target !== undefined) {
+    inputTarget.value = props.target.toString()
+  } else {
+    inputTarget.value = ''
+  }
+}
 </script>
