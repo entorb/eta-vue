59de67c72cb77d4336ad43b7506dafb5eaca94e6
Torben <59419684+entorb@users.noreply.github.com>
2023-10-25 04:04:18 +0200

    moved to entorb.net/eta

--- scripts/publish_to_web.sh
+++ scripts/publish_to_web.sh
@@ -5,6 +5,7 @@ script_dir=$(cd $(dirname $0) && pwd)
 cd $script_dir/..
 
 # npm run format && npm run lint && npm run test-once &&
-#     npm run build && rsync -rvhu --delete --no-perms dist/* entorb@entorb.net:html/eta-vue/
+#     npm run build && rsync -rvhu --delete --no-perms dist/* entorb@entorb.net:html/eta/
 
-npm run build && rsync -rhv --delete --exclude=v1 --no-perms --ignore-times dist/ entorb@entorb.net:html/eta-vue/
+# keep dir v1 untouched
+npm run build && rsync -rhv --delete --exclude=v1 --no-perms --ignore-times dist/ entorb@entorb.net:html/eta/
