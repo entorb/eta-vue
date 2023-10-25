f3dc25e2ac1659cb1efea63df495076fbf6ec317
Torben <59419684+entorb@users.noreply.github.com>
2023-10-25 04:04:18 +0200

    moved to entorb.net/eta

--- src/App.vue
+++ src/App.vue
@@ -30,8 +30,8 @@
             This app is OpenSource. Its source code and project documentation can be found at
             <a href="https://github.com/entorb/eta-vue/" target="_blank"
               >GitHub.com/entorb/etc-vue/</a
-            >. Feel free to
-            <a href="https://entorb.net/contact.php?origin=eta-vue" target="_blank">contact</a> me.
+            >. The previous version can be found at <a href="v1/">v1/</a>. Feel free to
+            <a href="https://entorb.net/contact.php?origin=eta" target="_blank">contact</a> me.
           </v-col>
         </v-row>
       </v-container>
