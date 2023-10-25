05fb7c1bdb9fe9aa504ccd90d216ff2dbb7cd377
Torben <59419684+entorb@users.noreply.github.com>
2023-10-25 04:59:05 +0200

    eslint: require spacing before functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:vue/vue3-recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'function' }
+    ]
   }
 }
