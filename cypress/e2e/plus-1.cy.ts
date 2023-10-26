bfb9fb722fe9828abe5d65779a0dfa3d23dba067
Torben <59419684+entorb@users.noreply.github.com>
2023-10-26 05:38:39 +0200

    first E2E tests

---
+++ cypress/e2e/plus-1.cy.ts
@@ -0,0 +1,59 @@
+import { getLocalStorageDataFistLastValue } from './helper-cy'
+
+describe('plus-1', () => {
+  beforeEach(() => {
+    cy.visit('eta/')
+  })
+
+  it('1 click on plus-1, target = none', () => {
+    // eslint-disable-next-line cypress/unsafe-to-chain-command
+    cy.get('#btn-plus-1')
+      .click()
+      .should(() => {
+        expect(localStorage.getItem('eta_vue_data')).not.to.be.null
+        const { last, first } = getLocalStorageDataFistLastValue()
+        expect(first).to.eq(1)
+        expect(last).to.eq(1)
+      })
+  })
+
+  it('1 click on plus-1, target = 10', () => {
+    cy.get('#input-target').type('10{enter}')
+    // eslint-disable-next-line cypress/unsafe-to-chain-command
+    cy.get('#btn-plus-1')
+      .click()
+      .should(() => {
+        expect(localStorage.getItem('eta_vue_target')).to.eq('10')
+        expect(localStorage.getItem('eta_vue_data')).not.to.be.null
+        const { last, first } = getLocalStorageDataFistLastValue()
+        expect(first).to.eq(1)
+        expect(last).to.eq(1)
+      })
+  })
+
+  it('1 click on plus-1, target = 0, value = 123', () => {
+    cy.get('#input-target').type('0{enter}')
+    cy.get('#input-value').type('123{enter}')
+
+    cy.get('#btn-plus-1').click()
+    cy.should(() => {
+      expect(localStorage.getItem('eta_vue_target')).to.eq('0')
+      expect(localStorage.getItem('eta_vue_data')).not.to.be.null
+      const { last, first } = getLocalStorageDataFistLastValue()
+      expect(first).to.eq(123)
+      expect(last).to.eq(122)
+    })
+  })
+
+  it('10 clicks on plus-1, target = none', () => {
+    for (let n = 0; n < 10; n++) {
+      cy.get('#btn-plus-1').click()
+    }
+    cy.should(() => {
+      expect(localStorage.getItem('eta_vue_data')).not.to.be.null
+      const { last, first } = getLocalStorageDataFistLastValue()
+      expect(first).to.eq(1)
+      expect(last).to.eq(10)
+    })
+  })
+})
