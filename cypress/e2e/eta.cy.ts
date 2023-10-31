feb6368be8b1cf9a04a3153cac09867c71943294
Torben <59419684+entorb@users.noreply.github.com>
2023-10-31 21:56:36 +0100

    E2E test add items

--- cypress/e2e/eta.cy.ts
+++ cypress/e2e/eta.cy.ts
@@ -1,4 +1,4 @@
-// import { getLocalStorageDataFistLastValue } from './helper-cy'
+import { getLocalStorageDataFistLastValue } from './helper-cy'
 
 describe('start app', () => {
   beforeEach(() => {
@@ -18,15 +18,6 @@ describe('set target', () => {
     cy.visit('eta/')
   })
 
-  it('sets target to local storage', () => {
-    // eslint-disable-next-line cypress/unsafe-to-chain-command
-    cy.get('#input-target')
-      .type('10{enter}')
-      .should(() => {
-        expect(localStorage.getItem('eta_vue_target')).to.eq('10')
-      })
-  })
-
   it('sets target=10 to local storage', () => {
     // eslint-disable-next-line cypress/unsafe-to-chain-command
     cy.get('#input-target')
@@ -37,6 +28,36 @@ describe('set target', () => {
         // var 2
         const target = localStorage.getItem('eta_vue_target')
         expect(target).to.eq('10')
+      })
+  })
+})
+
+describe('set items', () => {
+  beforeEach(() => {
+    cy.visit('eta/')
+  })
+
+  it('add 1 item', () => {
+    // eslint-disable-next-line cypress/unsafe-to-chain-command
+    cy.get('#input-value')
+      .type('1{enter}')
+      .should(() => {
+        const { first, last } = getLocalStorageDataFistLastValue()
+        expect(first).to.eq(1)
+        expect(last).to.eq(1)
+      })
+  })
+
+  it('add 3 items', () => {
+    // eslint-disable-next-line cypress/unsafe-to-chain-command
+    cy.get('#input-value')
+      .type('1{enter}')
+      .type('2{enter}')
+      .type('3{enter}')
+      .should(() => {
+        const { first, last } = getLocalStorageDataFistLastValue()
+        expect(first).to.eq(1)
+        expect(last).to.eq(3)
       })
   })
 })
