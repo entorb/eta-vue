dd0b0aa7d0deb2d21d77dfb2011373a5dd50bda1
Torben <59419684+entorb@users.noreply.github.com>
2023-11-01 01:13:14 +0100

    E2E tests for Multi-Timer

--- cypress/e2e/plus-1.cy.ts
+++ cypress/e2e/plus-1.cy.ts
@@ -1,4 +1,4 @@
-import { getLocalStorageDataFistLastRowItems } from './helper-cy'
+import { getEtaLocalStorageDataFistLastRowItems } from './helper-cy'
 
 describe('plus-1', () => {
   beforeEach(() => {
@@ -11,7 +11,7 @@ describe('plus-1', () => {
       .click()
       .should(() => {
         expect(localStorage.getItem('eta_vue_data')).not.to.be.null
-        const { last, first } = getLocalStorageDataFistLastRowItems()
+        const { last, first } = getEtaLocalStorageDataFistLastRowItems()
         expect(first).to.eq(1)
         expect(last).to.eq(1)
       })
@@ -25,7 +25,7 @@ describe('plus-1', () => {
       .should(() => {
         expect(localStorage.getItem('eta_vue_target')).to.eq('10')
         expect(localStorage.getItem('eta_vue_data')).not.to.be.null
-        const { last, first } = getLocalStorageDataFistLastRowItems()
+        const { last, first } = getEtaLocalStorageDataFistLastRowItems()
         expect(first).to.eq(1)
         expect(last).to.eq(1)
       })
@@ -39,7 +39,7 @@ describe('plus-1', () => {
     cy.should(() => {
       expect(localStorage.getItem('eta_vue_target')).to.eq('0')
       expect(localStorage.getItem('eta_vue_data')).not.to.be.null
-      const { last, first } = getLocalStorageDataFistLastRowItems()
+      const { last, first } = getEtaLocalStorageDataFistLastRowItems()
       expect(first).to.eq(123)
       expect(last).to.eq(122)
     })
@@ -51,7 +51,7 @@ describe('plus-1', () => {
     }
     cy.should(() => {
       expect(localStorage.getItem('eta_vue_data')).not.to.be.null
-      const { last, first } = getLocalStorageDataFistLastRowItems()
+      const { last, first } = getEtaLocalStorageDataFistLastRowItems()
       expect(first).to.eq(1)
       expect(last).to.eq(10)
     })
