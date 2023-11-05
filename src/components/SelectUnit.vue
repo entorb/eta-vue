8a8655e1032b4b5ce3ddd3e24314c25c78be7a85
Torben <59419684+entorb@users.noreply.github.com>
2023-11-05 06:27:59 +0100

    ActionsBlock -> SelectUnit

---
+++ src/components/SelectUnit.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-select
+    id="select-unit"
+    v-model="unitSelected"
+    label="Speed unit"
+    variant="outlined"
+    :items="unitList"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const emits = defineEmits(['unit'])
+
+const props = defineProps({
+  unitInitial: { type: String, required: true }
+})
+
+const unitList = ref(['sec', 'min', 'hour', 'day'])
+const unitSelected = ref(props.unitInitial)
+
+watch(unitSelected, () => {
+  emits('unit', unitSelected)
+})
+</script>
