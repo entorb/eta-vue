c4ea4d2a35a26c33169e8a052fc1033377f9474a
Torben <59419684+entorb@users.noreply.github.com>
2023-11-12 05:25:52 +0100

    E2E tests: using select options names and some more tests

--- cypress/e2e/plus-1.cy.ts
+++ cypress/e2e/plus-1.cy.ts
@@ -3,11 +3,11 @@ import { getEtaLocalStorageDataFistLastRowItems } from './helper-cy'
 describe('plus-1', () => {
   beforeEach(() => {
     cy.visit('eta/')
+    cy.get('#select-mode').parent().click()
   })
 
   it('1 click on plus-1, target = none', () => {
-    cy.get('#select-mode').parent().click()
-    cy.get('.v-list > :nth-child(4)').click()
+    cy.get('.v-list-item__content').contains('Simple').click()
     // eslint-disable-next-line cypress/unsafe-to-chain-command
     cy.get('#btn-plus-1')
       .click()
@@ -20,8 +20,7 @@ describe('plus-1', () => {
   })
 
   it('1 click on plus-1, target = 10', () => {
-    cy.get('#select-mode').parent().click()
-    cy.get('.v-list > :nth-child(3)').click()
+    cy.get('.v-list-item__content').contains('Up').click()
     // eslint-disable-next-line cypress/unsafe-to-chain-command
     cy.get('#input-target').clear().type('10{enter}')
     // eslint-disable-next-line cypress/unsafe-to-chain-command
@@ -37,8 +36,7 @@ describe('plus-1', () => {
   })
 
   it('1 click on plus-1, target = 0, items = 123', () => {
-    cy.get('#select-mode').parent().click()
-    cy.get('.v-list > :nth-child(2)').click()
+    cy.get('.v-list-item__content').contains('Down').click()
     cy.get('#input-items').type('123{enter}')
 
     cy.get('#btn-plus-1').click()
@@ -52,8 +50,7 @@ describe('plus-1', () => {
   })
 
   it('10 clicks on plus-1, target = none', () => {
-    cy.get('#select-mode').parent().click()
-    cy.get('.v-list > :nth-child(4)').click()
+    cy.get('.v-list-item__content').contains('Simple').click()
     for (let n = 0; n < 10; n++) {
       cy.get('#btn-plus-1').click()
     }
