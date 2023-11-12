dc6c4038dc64807df6415baf48324b1d0dc0ff6b
Torben <59419684+entorb@users.noreply.github.com>
2023-11-12 03:34:08 +0100

    router.ts: lazy loading of tabs via defineAsyncComponent

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,12 @@
+import { defineAsyncComponent } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import MainEta from '@/components/MainEta.vue'
-import MainMultiTimer from '@/components/MainMultiTimer.vue'
-import MainInfo from '@/components/MainInfo.vue'
+// import MainEta from '@/components/MainEta.vue'
+// import MainMultiTimer from '@/components/MainMultiTimer.vue'
+// import MainInfo from '@/components/MainInfo.vue'
+// Lazy loading of tabs to speed up initial loading
+const MainEta = defineAsyncComponent(() => import('@/components/MainEta.vue'))
+const MainMultiTimer = defineAsyncComponent(() => import('@/components/MainMultiTimer.vue'))
+const MainInfo = defineAsyncComponent(() => import('@/components/MainInfo.vue'))
 
 export default createRouter({
   history: createWebHistory(),
