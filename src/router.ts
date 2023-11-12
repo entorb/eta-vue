c69aba5385763b2cc16dd42a2f0a81b247d9b68f
Torben <59419684+entorb@users.noreply.github.com>
2023-11-12 03:54:13 +0100

    router.ts: fix [Vue Router warn]: Component "default" in record with path is defined using "defineAsyncComponent()"

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,13 @@
-import { defineAsyncComponent } from 'vue'
+// import { defineAsyncComponent } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 // import MainEta from '@/components/MainEta.vue'
 // import MainMultiTimer from '@/components/MainMultiTimer.vue'
 // import MainInfo from '@/components/MainInfo.vue'
 // Lazy loading of tabs to speed up initial loading
-const MainEta = defineAsyncComponent(() => import('@/components/MainEta.vue'))
-const MainMultiTimer = defineAsyncComponent(() => import('@/components/MainMultiTimer.vue'))
-const MainInfo = defineAsyncComponent(() => import('@/components/MainInfo.vue'))
+// here no defineAsyncComponent needed!
+const MainEta = () => import('@/components/MainEta.vue')
+const MainMultiTimer = () => import('@/components/MainMultiTimer.vue')
+const MainInfo = () => import('@/components/MainInfo.vue')
 
 export default createRouter({
   history: createWebHistory(),
