ad3d31ab6383862ae12f787b9ff4ff90da362049
Torben <59419684+entorb@users.noreply.github.com>
2025-03-23 20:13:24 +0100

    Drop mode selection and add more icons

--- cypress/e2e/eta.cy.ts
+++ cypress/e2e/eta.cy.ts
@@ -5,66 +5,38 @@ describe('start app', () => {
     cy.visit('eta/')
   })
 
-  it('passes', () => {
-    cy.get('#select-mode').parent().click()
-    cy.get('.v-list-item__content').contains('Up').click()
-    // cy.get('.v-list > :nth-child(3)').click()
-    cy.get('#input-target').clear().type('10{enter}')
-    cy.get('#input-items').type('1')
-    cy.get('#input-items').type('2{enter}')
-    cy.get('#btn-plus-1').click()
-  })
-})
-
-describe('mode count-up: set target', () => {
-  beforeEach(() => {
-    cy.visit('eta/')
-    cy.get('#select-mode').parent().click()
-    cy.get('.v-list-item__content').contains('Up').click()
-    // cy.get('.v-list > :nth-child(3)').click()
-  })
-
-  it('sets target=20 to local storage', () => {
+  it('Mode Up: check target in local storage', () => {
     cy.get('#input-target')
       .clear()
       .type('20{enter}')
       .should(() => {
-        // var 1
         expect(localStorage.getItem('eta_vue_target')).to.eq('20')
-        // var 2
-        const target = localStorage.getItem('eta_vue_target')
-        expect(target).to.eq('20')
       })
-    cy.get('#input-items').type('3{enter}')
-  })
-})
 
-describe('mode count-down: set items', () => {
-  beforeEach(() => {
-    cy.visit('eta/')
-    cy.get('#select-mode').parent().click()
-    cy.get('.v-list-item__content').contains('Down').click()
-  })
-
-  it('add 1 item', () => {
-    cy.get('#input-items')
-      .type('1{enter}')
+    cy.get('#input-items').type('1{enter}')
+    cy.get('#input-items').type('12{enter}')
+    cy.get('#btn-plus-1')
+      .click()
       .should(() => {
         const { first, last } = getEtaLocalStorageDataFistLastRowItems()
         expect(first).to.eq(1)
-        expect(last).to.eq(1)
+        expect(last).to.eq(12+1)
       })
   })
 
-  it('add 3 items', () => {
+  it('Down: simple', () => {
+    cy.get('#input-target')
+      .clear()
+      .type('0{enter}')
+      // stored into local storage only after entering of items
+
     cy.get('#input-items')
-      .type('3{enter}')
-      .type('2{enter}')
-      .type('1{enter}')
+      .type('13{enter}')
       .should(() => {
+        expect(localStorage.getItem('eta_vue_target')).to.eq('0')
         const { first, last } = getEtaLocalStorageDataFistLastRowItems()
-        expect(first).to.eq(3)
-        expect(last).to.eq(1)
+        expect(first).to.eq(13)
+        expect(last).to.eq(13)
       })
   })
 })
