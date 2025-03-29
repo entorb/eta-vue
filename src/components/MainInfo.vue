78561e724533027b5a82833f9846559ec37e617b
Torben <59419684+entorb@users.noreply.github.com>
2025-03-29 07:46:28 +0100

    Update MainInfo.vue

--- src/components/MainInfo.vue
+++ src/components/MainInfo.vue
@@ -2,38 +2,9 @@
   <v-container>
     <v-row>
       <v-col>
-        <h2>How to install as app</h2>
-        <p style="padding-bottom: 10px">
-          <a href="https://en.wikipedia.org/wiki/Progressive_web_app" target="_blank">PWA</a>
-          technology is used to enable app installation.
-        </p>
-        <p>
-          <strong>Android</strong><br />
-          Open <a href="https://entorb.net/eta/" target="_blank">ETA</a> in Edge or Chrome -> Tap
-          Install
-        </p>
-        <p>
-          <strong>iOS (iPhone, iPad)</strong><br />
-          Open <a href="https://entorb.net/eta/" target="_blank">ETA</a> in Safari -> click share
-          button at button of page -> choose "Add to Home Screen"
-        </p>
-        <p>
-          <strong>MacOS</strong><br />
-          Open <a href="https://entorb.net/eta/" target="_blank">ETA</a> in Safari -> Menu -> File
-          -> Add to Dock
-        </p>
-        <p>
-          <strong>Windows</strong><br />
-          Open <a href="https://entorb.net/eta/" target="_blank">ETA</a> in Edge or Chrome ->
-          Address bar, right side -> Install button
-        </p>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
         <h2>Remaining-Timer / ETA</h2>
         <p style="padding-bottom: 10px">
-          Calculate remaining (waiting) time / estimated time of arrival.
+          Calculate remaining (waiting) time / estimated time of arrival (ETA).
         </p>
         <p>
           <strong>Count-down</strong> (target = 0)<br />
@@ -66,6 +37,35 @@
       <v-col>
         <h2>Multi-Timer</h2>
         <p>Create an arbitrary number of countdown timers.</p>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h2>How to install as app</h2>
+        <p style="padding-bottom: 10px">
+          <a href="https://en.wikipedia.org/wiki/Progressive_web_app" target="_blank">PWA</a>
+          technology is used to enable app installation.
+        </p>
+        <p>
+          <strong>Android</strong><br />
+          Open <a href="https://entorb.net/eta/" target="_blank">ETA</a> in Edge or Chrome -> Tap
+          Install
+        </p>
+        <p>
+          <strong>iOS (iPhone, iPad)</strong><br />
+          Open <a href="https://entorb.net/eta/" target="_blank">ETA</a> in Safari -> click share
+          button at button of page -> choose "Add to Home Screen"
+        </p>
+        <p>
+          <strong>MacOS</strong><br />
+          Open <a href="https://entorb.net/eta/" target="_blank">ETA</a> in Safari -> Menu -> File
+          -> Add to Dock
+        </p>
+        <p>
+          <strong>Windows</strong><br />
+          Open <a href="https://entorb.net/eta/" target="_blank">ETA</a> in Edge or Chrome ->
+          Address bar, right side -> Install button
+        </p>
       </v-col>
     </v-row>
     <v-row>
