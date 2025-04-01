1bcb2173e23e81cacfeea9cebc1963c7a1ece034
Torben <59419684+entorb@users.noreply.github.com>
2025-04-01 07:11:09 +0200

    Fix SonarQube findings

--- src/components/__tests__/StatsTable.spec.ts
+++ src/components/__tests__/StatsTable.spec.ts
@@ -2,16 +2,12 @@ import { describe, it, expect, beforeEach } from 'vitest'
 import { shallowMount } from '@vue/test-utils'
 import StatsTable from '../StatsTable.vue'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function initializeWrapper(wrapper) {}
-
 describe('Component renders', () => {
   let wrapper
   beforeEach(() => {
     wrapper = shallowMount(StatsTable, {
       props: { data: [], settings: { showDays: false, unitSpeed: 'min' } }
     })
-    initializeWrapper(wrapper)
   })
   it('renders', () => {
     expect(wrapper.exists()).toBe(true)
