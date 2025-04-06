d899ff579bbc2b0050252cfd0804f49be8602c07
Torben <59419684+entorb@users.noreply.github.com>
2025-04-06 14:26:26 +0200

    Add unit tests for multitimer

--- src/components/__tests__/MainMultiTimer.spec.ts
+++ src/components/__tests__/MainMultiTimer.spec.ts
@@ -19,7 +19,7 @@ describe('Component renders', () => {
   })
 })
 
-describe('recentTimerStuff', () => {
+describe('timer naming', () => {
   let wrapper
   beforeEach(() => {
     wrapper = shallowMount(MainMultiTimer)
@@ -36,5 +36,77 @@ describe('recentTimerStuff', () => {
       time: 123,
       unit: 'hour'
     })
+  })
+})
+
+describe('timer creation', () => {
+  let wrapper
+  beforeEach(async () => {
+    wrapper = shallowMount(MainMultiTimer)
+    initializeWrapper(wrapper)
+    await wrapper.vm.deleteAll()
+  })
+
+  it('addViaInput 20', async () => {
+    wrapper.vm.inputName = 'Timer20m'
+    wrapper.vm.inputTime = '20m'
+    await wrapper.vm.addViaInput()
+    // console.log(wrapper.vm.data)
+    expect(wrapper.vm.data[0].name).toEqual('Timer20m')
+    expect(Math.round(wrapper.vm.data[0].remainingTime)).toEqual(1200)
+  })
+
+  it('addViaInput 2.5', async () => {
+    wrapper.vm.inputName = 'Timer2.5m'
+    wrapper.vm.inputTime = '2,5'
+    await wrapper.vm.addViaInput()
+    // console.log(wrapper.vm.data)
+    expect(wrapper.vm.data[0].name).toEqual('Timer2.5m')
+    expect(Math.round(wrapper.vm.data[0].remainingTime)).toEqual(150)
+  })
+
+  it('addViaInput 2,5 unnamed', async () => {
+    wrapper.vm.inputName = ''
+    wrapper.vm.inputTime = '2,5'
+    await wrapper.vm.addViaInput()
+    console.log(wrapper.vm.data)
+    expect(wrapper.vm.data[0].name).toEqual('Timer')
+    expect(Math.round(wrapper.vm.data[0].remainingTime)).toEqual(150)
+  })
+
+  it('addViaInput 10s', async () => {
+    wrapper.vm.inputName = 'Timer10s'
+    wrapper.vm.inputTime = '10s'
+    await wrapper.vm.addViaInput()
+    expect(wrapper.vm.data[0].name).toEqual('Timer10s')
+    expect(Math.round(wrapper.vm.data[0].remainingTime)).toEqual(10)
+    // console.log(wrapper.vm.data)
+  })
+
+  it('addViaInput 20m', async () => {
+    wrapper.vm.inputName = 'Timer20m'
+    wrapper.vm.inputTime = '20m'
+    await wrapper.vm.addViaInput()
+    console.log(wrapper.vm.data)
+    expect(wrapper.vm.data[0].name).toEqual('Timer20m')
+    expect(Math.round(wrapper.vm.data[0].remainingTime)).toEqual(1200)
+  })
+
+  it('addViaInput 3h', async () => {
+    wrapper.vm.inputName = 'Timer3h'
+    wrapper.vm.inputTime = '3h'
+    await wrapper.vm.addViaInput()
+    console.log(wrapper.vm.data)
+    expect(wrapper.vm.data[0].name).toEqual('Timer3h')
+    expect(Math.round(wrapper.vm.data[0].remainingTime)).toEqual(3 * 3600)
+  })
+
+  it('addViaInput 2d', async () => {
+    wrapper.vm.inputName = 'Timer2d'
+    wrapper.vm.inputTime = '2d'
+    await wrapper.vm.addViaInput()
+    console.log(wrapper.vm.data)
+    expect(wrapper.vm.data[0].name).toEqual('Timer2d')
+    expect(Math.round(wrapper.vm.data[0].remainingTime)).toEqual(2 * 24 * 3600)
   })
 })
