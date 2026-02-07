8eefce0102280700c5c5c40d946bc03af863e5dc
Torben <59419684+entorb@users.noreply.github.com>
2026-02-07 21:56:37 +0100

    Apply and fixing new eslint rules

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@
  */
 
 import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
 
 import { registerPlugins } from '@/plugins'
+
+import App from './App.vue'
+import router from './router'
+
 
 const app = createApp(App).use(router)
 
